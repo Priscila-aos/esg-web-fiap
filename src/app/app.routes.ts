1d08a82ff3c713b86258eb6536c1c13a587b0cbd
ericaokamura <erica.okamura@yahoo.com.br>
2023-12-10 22:59:43 -0300

    fix: ajustes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [ 
 
-    //é aqui Pri --> rota para linlar o formulario com o historico (mural)
+    //é aqui Pri --> rota para linkar o formulario com o historico (mural)
 
 
 ];
